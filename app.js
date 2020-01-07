1d63987a7cd930bb57376b38c5f28983918d0b19
Mrigank Anand <53966291+spiderxm@users.noreply.github.com>
2020-01-07 23:49:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //app.js
+//search input
+const github = new Github;
+const searchuser = document.getElementById('search-user');
+
+searchuser.addEventListener('keyup', (e) => {
+    const usertext = e.target.value;
+    if (usertext !== '') {
+        console.log(usertext);
+        github.getuser(usertext)
+            .then(data => {
+                console.log(data);
+            })
+    }
+});
