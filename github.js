1a806f53b9a96ed9477aa69a45c7ada4016b6c09
Mrigank Anand <53966291+spiderxm@users.noreply.github.com>
2020-01-07 23:49:42 +0530

    Update github.js

--- github.js
+++ github.js
@@ -1,1 +1,16 @@
+class Github {
+    constructor() {
+    }
 
+    async getuser(user) {
+            const abc = "https://api.github.com/users/spiderxm";
+        const profileresponse = await fetch(abc);
+        const profiledata = await profileresponse.json();
+        return {
+
+            profile: profiledata
+
+        }
+
+    }
+}
