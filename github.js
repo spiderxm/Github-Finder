9510df55cca42b37fc232c704f60336aae054b59
Mrigank Anand <53966291+spiderxm@users.noreply.github.com>
2020-01-10 18:10:14 +0530

    Add files via upload
    github.js

--- github.js
+++ github.js
@@ -1,11 +1,10 @@
 class Github {
     constructor() {
-  
+
     }
 
     async getuser(user) {
-        const abc = `https://api.github.com/users/${user}`;
-        const profileresponse = await fetch(abc);
+        const profileresponse = await fetch(`https://api.github.com/users/${user}`);
         const profiledata = await profileresponse.json();
         return {
 
